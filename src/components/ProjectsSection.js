cd6cd3ce4c7e877337af62dfaabce5740ba992b5
beaucoburn <douglas.coburn@gmail.com>
2024-01-23 17:24:25 -0600

    Add April Art to the projects section

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -5,28 +5,16 @@ import Card from "./Card";
 
 const projects = [
   {
-    title: "React Space",
+    title: "April Elizabeth E-Commerce NextJS App",
     description:
-      "Handy tool belt to create amazing AR components in a React app, with redux integration via middleware️",
-    getImageSrc: () => require("../images/photo1.jpg"),
+      "An E-Commerce app built with NextJS, React, Tailwind CSS, and Shopify's GraphQL API.",
+    getImageSrc: () => require("../images/aprilart.png"),
   },
   {
     title: "React Infinite Scroll",
     description:
       "A scrollable bottom sheet with virtualization support, native animations at 60 FPS and fully implemented in JS land 🔥️",
     getImageSrc: () => require("../images/photo2.jpg"),
-  },
-  {
-    title: "Photo Gallery",
-    description:
-      "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income",
-    getImageSrc: () => require("../images/photo3.jpg"),
-  },
-  {
-    title: "Event planner",
-    description:
-      "A mobile application for leisure seekers to discover unique events and activities in their city with a few taps",
-    getImageSrc: () => require("../images/photo4.jpg"),
   },
 ];
 
