047929b1975a7a667d69343b67e096bd2df77573
beaucoburn <douglas.coburn@gmail.com>
2024-01-26 05:59:40 -0600

    Remove Contact Section until ready

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Header from "./components/Header";
 import LandingSection from "./components/LandingSection";
 import ProjectsSection from "./components/ProjectsSection";
-import ContactMeSection from "./components/ContactMeSection";
+//import ContactMeSection from "./components/ContactMeSection";
 import Footer from "./components/Footer";
 import { AlertProvider } from "./context/alertContext";
 import Alert from "./components/Alert";
@@ -17,7 +17,7 @@ function App() {
           <LandingSection />
           <About />
           <ProjectsSection />
-          <ContactMeSection />
+          
           <Footer />
           <Alert />
         </main>
